be121065d7e76d3ba0e2405f9d67161a62d20f66
Taylor <96550502+taylors42@users.noreply.github.com>
2023-12-27 19:12:08 -0300

    added react-route and 404 page

--- src/components/Dash.jsx
+++ src/components/Dash.jsx
@@ -1,9 +1,15 @@
+import resume from '../resources/taylorResume.pdf';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 function Dash () {
     return (
         <ul className=" flex md:flex-col max-md:items-center max-md:justify-center text-white lg:mt-14 gap-3 duration-500 max-lg:text-xl max-lg:text-center 2xl:text-xl">
-            <li className=" duration-500 hover:scale-105 hover:font-semibold cursor-pointer">About</li>
-            <li className=" duration-500 hover:scale-105 hover:font-semibold cursor-pointer">Experience</li>
-            <li className=" duration-500 hover:scale-105 hover:font-semibold cursor-pointer">Projects</li>
+            <li className=" duration-500 hover:scale-105 hover:font-semibold cursor-pointer">
+                <Link to="/AboutPage">About</Link>
+                </li>
+            <li className=" duration-500 hover:scale-105 hover:font-semibold cursor-pointer">
+            <a href={resume} target="_blank" rel="noopener noreferrer">Experience</a></li>
+            <li className=" duration-500 hover:scale-105 hover:font-semibold cursor-pointer">
+            <a href="https://github.com/taylors42" target="_blank" rel="noopener noreferrer">Projects</a></li>
         </ul>
     )
 }
