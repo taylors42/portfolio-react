d9483c8a61f970f614b6b397da0590cd77d2c6a4
Taylor <96550502+taylors42@users.noreply.github.com>
2024-06-02 19:06:11 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import GnomeWindow from "./components/GnomeWindow";
 function App() {
   return (
     <div className=" my-8 mx-8 flex flex-col select-none justify-center">
-      <div className="flex justify-between">
-        <div className="flex items-center">
+      <div className="flex justify-between bg-neutral-500/20 rounded-xl p-2 items-center mb-4 shadow-lg shadow-black/40 max-sm:justify-center">
+        <div className="flex items-center ">
           <div className="h-14 w-14">
             <Avatar />
           </div>
@@ -15,14 +15,13 @@ function App() {
             <p className="text-white/70 font-light -mt-1 ">Programador Web</p>
           </div>
         </div>
-        <div className="">
+        <div className=" max-sm:hidden">
           <Menu />
         </div>
       </div>
-      <div className="flex items-center justify-between mt-4">
-        <div></div>
-        <GnomeWindow />
-        <div></div>
+      <div className="grid grid-cols-2 mt-4">
+        <div className=" bg-black max-h-[30rem] max-w-[20rem]"></div>
+        <div className=" bg-white max-h-[30rem] max-w-[60rem] min-w-[20rem]"></div>
       </div>
     </div>
   );
